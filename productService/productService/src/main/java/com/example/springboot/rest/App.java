7d06c707fe688da29891476a5a673b797334a632
Angel Plaza <angel.plaza@gmail.com>
2018-10-25 13:04:14 +0200

    Spring Boot Rest Template

--- productService/productService/src/main/java/com/example/springboot/rest/App.java
+++ productService/productService/src/main/java/com/example/springboot/rest/App.java
@@ -2,7 +2,8 @@ package com.example.springboot.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * REST API Product Service App
@@ -15,4 +16,10 @@ public class App {
     public static void main( String[] args ) {
         SpringApplication.run(App.class, args);
     }
+    
+    @Bean
+    public RestTemplate getRestTemplate() {
+       return new RestTemplate();
+    }
 }
+
