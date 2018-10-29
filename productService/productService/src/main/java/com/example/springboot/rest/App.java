d0ba5476fca8ae648b50c168bc2a8dd90e2788af
Angel Plaza <angel.plaza@gmail.com>
2018-10-29 15:47:49 +0100

    Spring Boot - CORS Support

--- productService/productService/src/main/java/com/example/springboot/rest/App.java
+++ productService/productService/src/main/java/com/example/springboot/rest/App.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * REST API Product Service App
@@ -20,6 +23,18 @@ public class App {
     @Bean
     public RestTemplate getRestTemplate() {
        return new RestTemplate();
+    }
+    
+    @Bean
+    @SuppressWarnings(value = { "deprecation" })
+    public WebMvcConfigurer corsConfigurer() {
+       return new WebMvcConfigurerAdapter() {
+          @Override
+          public void addCorsMappings(CorsRegistry registry) {
+        	  // Se permite CORS para app Angular
+             registry.addMapping("/products").allowedOrigins("http://localhost:4200");
+          }
+       };
     }
 }
 
